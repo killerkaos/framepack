0ef15f17f88c2276ace7743a541c868d1c009fc1
killerkaos <gesthal@gmail.com>
2024-01-21 23:08:03 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-echo "=========================================" > /proc/1/fd/1
-echo "MAKING THE FOLDERS AND LINKING THEM" > /proc/1/fd/1
-echo "=========================================" > /proc/1/fd/1
-
-if [ ! -d /config/miniconda ]; then
-    echo "No miniconda folder detected, will create" > /proc/1/fd/1
-    ln -s /root/miniconda /config/miniconda/
-    else
-    echo "Miniconda folder detected, skip" > /proc/1/fd/1
-fi
-
-if [ ! -d /config/text-generation-webui ]; then
-    echo "No text-generation-webui folder detected, will create" > /proc/1/fd/1
-    ln -s /root/text-generation-webui /config/text-generation-webui/ > /proc/1/fd/1
-else
-    echo "Text-generation-webui folder detected, skip" > /proc/1/fd/1
-fi
-
 # Execute installer script
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLER script" > /proc/1/fd/1

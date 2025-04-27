9a01257fef01a917e1203d31c45171bdccf74450
killerkaos <gesthal@gmail.com>
2025-04-27 18:14:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 conda init bash > /proc/1/fd/1
 source ~/.bashrc
 
-conda activate /home/abc/venv
+conda activate /config/venv
 
 echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING GRADIO SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /home/abc/FramePack
+cd /config/FramePack
 python demo_gradio.py --share > /proc/1/fd/1

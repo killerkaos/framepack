7e2814ba8b1d6f8322adc8648858febbec30361d
killerkaos <gesthal@gmail.com>
2025-04-27 17:40:41 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+v#!/bin/bash
 conda init bash > /proc/1/fd/1
 source ~/.bashrc
 
-if [ ! -d /home/abc/framepack ]; then
+if [ ! -d /home/abc/venv ]; then
 echo "=========================================" > /proc/1/fd/1
-echo "CONDA CREATE FRAMEPACK" > /proc/1/fd/1
+echo "CONDA CREATE venv" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-    conda create -p /home/abc/framepack python=3.10 -y > /proc/1/fd/1
+    conda create -p /home/abc/venv python=3.10 -y > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-echo "CONDA ACTIVATE FRAMEPACK" > /proc/1/fd/1
+echo "CONDA ACTIVATE venv" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-    conda activate /home/abc/framepack > /proc/1/fd/1
+    conda activate /home/abc/venv > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLING DEPENDENCIES: Torch torchvision torchaudio cuda126 (WILL TAKE A LONG TIME)" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
@@ -20,13 +20,13 @@ echo "DEPENDENCIES ALL INSTALLED" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 fi
 
-if [ ! -d /home/abc/text-generation-webui ]; then
+if [ ! -d /home/abc/framepack ]; then
 echo "=========================================" > /proc/1/fd/1
-echo "INSTALLING & Downloading text-GUI" > /proc/1/fd/1
+echo "INSTALLING & Downloading framepack" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 cd /home/abc/
-git clone https://github.com/oobabooga/text-generation-webui.git > /proc/1/fd/1
-cd text-generation-webui
+git clone https://github.com/lllyasviel/FramePack.git > /proc/1/fd/1
+cd FramePack
 pip install -r requirements.txt > /proc/1/fd/1
 fi
 

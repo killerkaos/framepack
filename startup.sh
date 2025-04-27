42b4b3d31b0bddf263895c129607aae7b8c1b866
killerkaos <gesthal@gmail.com>
2025-04-27 18:07:42 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING GRADIO SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 cd /home/abc/FramePack
-python demo_gradio.py --share --server-name > /proc/1/fd/1
+python demo_gradio.py --share > /proc/1/fd/1

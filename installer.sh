e77948d4526394eaf28f518ad0b2afaac20a27c2
killerkaos <gesthal@gmail.com>
2025-04-27 18:13:43 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,15 +2,15 @@
 conda init bash > /proc/1/fd/1
 source ~/.bashrc
 
-if [ ! -d /home/abc/venv ]; then
+if [ ! -d /config/venv ]; then
 echo "=========================================" > /proc/1/fd/1
 echo "CONDA CREATE venv" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-    conda create -p /home/abc/venv python=3.10 -y > /proc/1/fd/1
+    conda create -p /config/venv python=3.10 -y > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 echo "CONDA ACTIVATE venv" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-    conda activate /home/abc/venv > /proc/1/fd/1
+    conda activate /config/venv > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLING DEPENDENCIES: Torch torchvision torchaudio cuda126 (WILL TAKE A LONG TIME)" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
@@ -21,11 +21,11 @@ echo "DEPENDENCIES ALL INSTALLED" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 fi
 
-if [ ! -d /home/abc/framepack ]; then
+if [ ! -d /config/framepack ]; then
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLING & Downloading framepack" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /home/abc/
+cd /config/
 git clone https://github.com/lllyasviel/FramePack.git > /proc/1/fd/1
 cd FramePack
 pip install -r requirements.txt > /proc/1/fd/1
@@ -34,5 +34,5 @@ fi
 echo "=========================================" > /proc/1/fd/1
 echo "FINISH INSTALLATION" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /home/abc/
+cd /config/
 touch installed

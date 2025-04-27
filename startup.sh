1237b057680f9db03b683fef3a8b4cd5be1dadae
killerkaos <gesthal@gmail.com>
2025-04-27 17:48:18 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 conda init bash > /proc/1/fd/1
 source ~/.bashrc
 
-conda activate /home/abc/vicuna
+conda activate /home/abc/venv
 
 echo "=========================================" > /proc/1/fd/1
-echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
+echo "EXECUTING GRADIO SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /home/abc/text-generation-webui
-python server.py --listen > /proc/1/fd/1
+cd /home/abc/FramePack
+python demo_gradio.py --share --server-name > /proc/1/fd/1
